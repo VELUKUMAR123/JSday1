ebee046da38143638bbfcda1ce59091010339309
VELUKUMAR123 <140575906+VELUKUMAR123@users.noreply.github.com>
2023-07-26 16:18:15 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,11 @@
+
+// array    //assignment 1
+let fruits=[];                              //1
+let fruits = ['apple', 'banana', 'orange']; //2
+fruits.shift(0)                             //3
+console.log(fruits)
+fruits.push('grape')                        //4
+console.log(fruits)
+fruits[2]="pear"
+console.log(fruits)                         //5
+console.log(typeof fruits)
